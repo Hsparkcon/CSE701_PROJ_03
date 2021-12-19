d5ad8b3a3e6a534a3a52a1f9deef381d0f63e846
Hong Sung Park <hsparkcon@gmail.com>
2021-12-18 23:24:36 -0500

    Commit after documenting RGB_COLOUR

--- include/rgb_colour.hpp
+++ include/rgb_colour.hpp
@@ -1,6 +1,27 @@
 #pragma once
 #include "PCH.hpp"
 
+/**
+ * @brief
+ * RGB_COLOUR struct is user-defined struct used to store RGB colour data of pixel.
+ *
+ * @details
+ * Each colour channel uses uint8_t to data of store a value.
+ * RGB stores colour data by using three colour channels
+ * - RED
+ * - GREEN
+ * - BLUE
+ *
+ * Each channel stores an integer value range between 0 to 255.
+ * When each channel's value is near 0, the colour becomes black.
+ * When each channel's value is near 255, the colour becomes white.
+ * When one channel has a value near 255 while the other is near 0,
+ * the colour becomes the colour of the channel with a high value.
+ *
+ * For more detail, please read
+ * https://en.wikipedia.org/wiki/RGB_color_model
+ *
+ */
 struct RGB_COLOUR
 {
 private:
@@ -9,11 +30,22 @@ private:
     uint8_t m_blue;
 
 public:
+    /**
+     * @brief Construct a new RGB_COLOUR object initialized with black colour.
+     *
+     */
     RGB_COLOUR()
     {
         this->set_black();
     }
 
+    /**
+     * @brief Construct a new RGB_COLOUR object initialized with given colour data.
+     *
+     * @param red value of the red colour channel
+     * @param green value of the green colour channel
+     * @param blue value of the blue colour channel
+     */
     RGB_COLOUR(uint8_t red, uint8_t green, uint8_t blue)
     {
         this->set_colour(red, green, blue);
@@ -21,6 +53,13 @@ public:
 
     ~RGB_COLOUR() {}
 
+    /**
+     * @brief Set the colour value with given colour data.
+     *
+     * @param red value of the red colour channel
+     * @param green value of the green colour channel
+     * @param blue value of the blue colour channel
+     */
     void set_colour(uint8_t red, uint8_t green, uint8_t blue)
     {
         m_red = red;
@@ -28,6 +67,10 @@ public:
         m_blue = blue;
     }
 
+    /**
+     * @brief Set the colour value to black.
+     *
+     */
     void set_black()
     {
         m_red = 0;
@@ -35,6 +78,10 @@ public:
         m_blue = 0;
     }
 
+    /**
+     * @brief Set the colour value to white.
+     *
+     */
     void set_white()
     {
         m_red = 255;
@@ -42,16 +89,31 @@ public:
         m_blue = 255;
     }
 
+    /**
+     * @brief Returns the value of the red colour channel.
+     *
+     * @return uint8_t
+     */
     uint8_t get_red() const
     {
         return this->m_red;
     }
 
+    /**
+     * @brief Returns the value of the green colour channel.
+     *
+     * @return uint8_t
+     */
     uint8_t get_green() const
     {
         return this->m_green;
     }
 
+    /**
+     * @brief Returns the value of the blue colour channel.
+     *
+     * @return uint8_t
+     */
     uint8_t get_blue() const
     {
         return this->m_blue;
